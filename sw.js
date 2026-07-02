f36d280aa5810d7c5eb3bb9e7ce968590b5f31f6
linc8763-ship-it <linc8763@gmail.com>
2026-07-02 18:36:23 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install', e=>e.waitUntil(caches.open('cm602-v1').then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest','./template.xlsx']))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
